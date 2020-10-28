9cb59940fcf6ebb74305f35c6c39c7b6f6f0efb2
Nate Weaver <wevah@derailer.org>
2020-10-28 13:36:36 -0500

    Rename CommandLine+Extensions to CLIFormat+Extensions

---
+++ Sources/CommandLineFormat/CLIFormat+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  CommandLine+Extensions.swift
+//  
+//
+//  Created by Nate Weaver on 2020-10-26.
+//
+
+import Foundation
+
+public extension CLIFormat {
+
+	/// Whether the terminal emulator supports 24-bit color, determined by the contents of the `COLORTERM`
+	/// environment variable.
+	static var supportsTrueColor: Bool {
+		guard let colorterm = ProcessInfo.processInfo.environment["COLORTERM"] else { return false }
+		return colorterm == "truecolor" || colorterm == "24bit"
+	}
+
+	/// Sets the terminal window title.
+	static func setWindowTitle(_ title: String) {
+		print("\(CLIFormat.escape)]0;\(title)\(CLIFormat.bell)")
+	}
+
+}
