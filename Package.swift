29713b619edc7cd61a2596c4b4d6815775195a39
Nate Weaver <wevah@derailer.org>
2020-10-28 14:55:51 -0500

    Add example target

--- Package.swift
+++ Package.swift
@@ -4,31 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "CommandLineFormat",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "CommandLineFormat",
-            targets: ["CommandLineFormat"]),
+	name: "CommandLineFormat",
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "CommandLineFormat",
+			targets: ["CommandLineFormat"]),
 		.library(
 			name: "CommandLineImage",
 			targets: ["CommandLineImage"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "CommandLineFormat",
-            dependencies: []),
+		.executable(name: "example", targets: ["Example"])
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "CommandLineFormat",
+			dependencies: []),
 		.target(
 			name: "CommandLineImage",
 			dependencies: ["CommandLineFormat"]),
-       .testTarget(
-            name: "CommandLineFormatTests",
-            dependencies: ["CommandLineFormat"]),
-    ]
+		.target(
+			name: "Example",
+			dependencies: ["CommandLineImage"]),
+		.testTarget(
+			name: "CommandLineFormatTests",
+			dependencies: ["CommandLineFormat"]),
+	]
 )
