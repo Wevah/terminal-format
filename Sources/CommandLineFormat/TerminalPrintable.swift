35954e757188114edb74f37698910389cd286af4
Nate Weaver <wevah@derailer.org>
2020-11-04 15:27:01 -0600

    Add TerminalPrintable protocol

---
+++ Sources/CommandLineFormat/TerminalPrintable.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nate Weaver on 2020-11-02.
+//
+
+import Foundation
+
+public protocol TerminalPrintable {
+
+	var escapeSequence: String { get }
+
+}
+
+public extension DefaultStringInterpolation {
+
+	mutating func appendInterpolation<T: TerminalPrintable>(_ printable: T) {
+		self.appendLiteral(printable.escapeSequence)
+	}
+
+	mutating func appendInterpolation<T: TerminalPrintable & CustomStringConvertible>(_ printable: T) {
+		self.appendLiteral(printable.escapeSequence)
+	}
+
+	mutating func appendInterpolation<T: TerminalPrintable>(_ printable: T?) {
+		guard let representable = printable else { return }
+		self.appendInterpolation(representable.escapeSequence)
+	}
+
+	mutating func appendInterpolation<T: TerminalPrintable & CustomStringConvertible>(_ printable: T?) {
+		guard let representable = printable else { return }
+		self.appendInterpolation(representable.escapeSequence)
+	}
+
+}
